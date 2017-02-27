4360c67d5cb87163c3f42f5a80b3ae6f9bade13c
Martin Huschenbett <martin-huschenbett@web.de>
2017-02-27 16:19:36 +0100

    some metorites are dropping down now; no collision

--- game.js
+++ game.js
@@ -26,13 +26,19 @@ MainGame.prototype = {
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		console.log("Fertig mit Laden");
 		this.spacefield = game.add.tileSprite(0,0,800,600,"spacefield");
-		this.player = game.add.sprite(0,0, "Player");
-		this.player.y = 300;
-		this.player.x = 400;
+		this.player = game.add.sprite(300,400, "Player");
+		this.metorites = Array(10);
 		this.btnUP = game.input.keyboard.addKey(Phaser.Keyboard.W);			
 		this.btnDOWN = game.input.keyboard.addKey(Phaser.Keyboard.S);
 		this.btnRIGHT = game.input.keyboard.addKey(Phaser.Keyboard.D);
 		this.btnLEFT = game.input.keyboard.addKey(Phaser.Keyboard.A);
+
+		for (var i = 0; i < this.metorites.length; i++) {
+			//var rand = game.rnd.realInRange(-10,1);
+			this.metorites[i] = game.add.sprite(game.world.randomX, -200 +(i*100), "Metorite");
+			this.metorites[i].scale.setTo(0.3,0.3);
+		}
+
 	},
 
 	update: function(){
@@ -41,5 +47,14 @@ MainGame.prototype = {
 		if(this.btnDOWN.isDown && this.player.y < 590 - 92) this.player.y += 5;
 		if(this.btnLEFT.isDown && this.player.x > 10) this.player.x -= 5;
 		if(this.btnRIGHT.isDown && this.player.x < 790 - 69) this.player.x += 5;
+
+		for(var i = 0; i < this.metorites.length; i++){
+			if(this.metorites[i].y < 600){
+				this.metorites[i].y += 5;
+			} else {
+				this.metorites[i].y = -400;
+				this.metorites[i].x = game.world.randomX;
+			}
+		}
 	}
 }
